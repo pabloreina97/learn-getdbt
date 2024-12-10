774500c9ea09595da3ea3481d8396c533a9c9507
Pablo Reina <pabloreinagalvez@gmail.com>
2024-12-10 11:28:40 +0100

    Quitar aliases innecesarios

--- dbt_refactor/models/marts/fct_customer_orders.sql
+++ dbt_refactor/models/marts/fct_customer_orders.sql
@@ -28,8 +28,8 @@ with
             c.first_name as customer_first_name,
             c.last_name as customer_last_name
         from orders
-        left join completed_payments as p on orders.id = p.order_id
-        left join customers c on orders.user_id = c.id
+        left join completed_payments on orders.id = completed_payments.order_id
+        left join customers on orders.user_id = customers.id
     ),
 
     customer_orders as (
@@ -38,30 +38,30 @@ with
             min(order_date) as first_order_date,
             max(order_date) as most_recent_order_date,
             count(orders.id) as number_of_orders
-        from customers as c
-        left join orders on orders.user_id = c.id
+        from customers
+        left join orders on orders.user_id = customers.id
         group by 1
     ),
 
     -- Final CTE
     final as (
         select
-            p.*,
-            row_number() over (order by p.order_id) as transaction_seq,
+            paid_orders.*,
+            row_number() over (order by paid_orders.order_id) as transaction_seq,
             row_number() over (
-                partition by customer_id order by p.order_id
+                partition by customer_id order by paid_orders.order_id
             ) as customer_sales_seq,
             case
-                when c.first_order_date = p.order_placed_at then 'new' else 'return'
+                when c.first_order_date = paid_orders.order_placed_at then 'new' else 'return'
             end as nvsr,
             x.clv_bad as customer_lifetime_value,
             sum(total_amount_paid) over (
                 partition by paid_orders.customer_id
                 order by paid_orders.order_placed_at
             ) as customer_lifetime_value
-            c.first_order_date as fdos
-        from paid_orders p
-        left join customer_orders as c using (customer_id)
+            customer_orders.first_order_date as fdos
+        from paid_orders
+        left join customer_orders using (customer_id)
         order by order_id
     ),
 
