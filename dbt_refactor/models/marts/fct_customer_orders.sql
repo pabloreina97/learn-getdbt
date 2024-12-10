f31105c09f9c85b2c1f05ecc8c668e1d31338eac
Pablo Reina <pabloreinagalvez@gmail.com>
2024-12-10 11:25:46 +0100

    Cambiar subquery por función ventana

--- dbt_refactor/models/marts/fct_customer_orders.sql
+++ dbt_refactor/models/marts/fct_customer_orders.sql
@@ -55,24 +55,16 @@ with
                 when c.first_order_date = p.order_placed_at then 'new' else 'return'
             end as nvsr,
             x.clv_bad as customer_lifetime_value,
+            sum(total_amount_paid) over (
+                partition by paid_orders.customer_id
+                order by paid_orders.order_placed_at
+            ) as customer_lifetime_value
             c.first_order_date as fdos
         from paid_orders p
         left join customer_orders as c using (customer_id)
-        left outer join
-            (
-                select p.order_id, sum(t2.total_amount_paid) as clv_bad
-                from paid_orders p
-                left join
-                    paid_orders t2
-                    on p.customer_id = t2.customer_id
-                    and p.order_id >= t2.order_id
-                group by 1
-                order by p.order_id
-            ) x
-            on x.order_id = p.order_id
         order by order_id
     ),
-    
+
 -- Simple Select Statment
 select *
 from final
