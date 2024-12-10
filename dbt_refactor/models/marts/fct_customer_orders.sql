5c0ec0eeb3989cfdaa13f15d70dec2fb13368ecc
Pablo Reina <pabloreinagalvez@gmail.com>
2024-12-10 09:56:39 +0100

    Modificado el source en el código de mart

--- dbt_refactor/models/marts/fct_customer_orders.sql
+++ dbt_refactor/models/marts/fct_customer_orders.sql
@@ -6,20 +6,20 @@ WITH paid_orders as (select Orders.ID as order_id,
     p.payment_finalized_date,
     C.FIRST_NAME    as customer_first_name,
         C.LAST_NAME as customer_last_name
-FROM jaffle_shop.jaffle_shop_orders as Orders
+FROM {{ source('jaffle_shop', 'orders') }} as Orders
 left join (select ORDERID as order_id, max(CREATED) as payment_finalized_date, sum(AMOUNT) / 100.0 as total_amount_paid
-        from jaffle_shop.stripe_payments
+        from {{ source('jaffle_shop', 'payments') }}
         where STATUS <> 'fail'
         group by 1) p ON orders.ID = p.order_id
-left join jaffle_shop.jaffle_shop_customers C on orders.USER_ID = C.ID ),
+left join {{ source('jaffle_shop', 'customers') }} C on orders.USER_ID = C.ID ),
 
 customer_orders 
 as (select C.ID as customer_id
     , min(ORDER_DATE) as first_order_date
     , max(ORDER_DATE) as most_recent_order_date
     , count(ORDERS.ID) AS number_of_orders
-from jaffle_shop.jaffle_shop_customers C 
-left join jaffle_shop.jaffle_shop_orders as Orders
+from {{ source('jaffle_shop', 'customers') }} C 
+left join {{ source('jaffle_shop', 'orders') }} as Orders
 on orders.USER_ID = C.ID 
 group by 1)
 
